9abdf13cc3868dbb24dc30e9bbb7cdd8117fc764
anirusso <ar.anitarusso@gmail.com>
2023-12-07 07:40:48 -0500

    updating script and style

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $(window).scroll(function(event) {
     let scroll = $(this).scrollTop();
-    let opacity = 1 - scroll / 1500;
+    let opacity = 1 - scroll / 750;
     if (opacity >= 0) {
       $('#top').css('opacity', opacity);
     }
